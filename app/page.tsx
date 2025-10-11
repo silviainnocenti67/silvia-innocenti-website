bf1c25d60ecd107be3cc56d35bde396f32f0a849
dcaon01 <d.caon01@gmail.com>
2025-10-11 12:52:41 +0200

    Change in the work in progress.

--- app/page.tsx
+++ app/page.tsx
@@ -18,9 +18,19 @@ const noto = Noto_Serif({
 export default function HomePage() {
   return (
     <div className={styles.container}>
+      {/* Contacts */}
+      <div className={styles.contacts}>
+        <p className={styles.contactsText}><strong>Email:</strong> <a href="mailto:silviainnocentiarch@gmail.com">silviainnocentiarch@gmail.com</a></p>
+        <p className={styles.contactsText}><strong>Telefono:</strong> <a href="tel:+393382137308">338 213 7308</a></p>
+      </div>
+
+      {/* Content */}
       <div className={styles.content}>
-        <h1 className={`${styles.title} ${noto.className}`}>Silvia Innocenti</h1>
-        <p className={styles.subtitle}>Presto online</p>
+        <div className={styles.textContainer}>
+          <p className={`${styles.profession} ${noto.className}`}>Architetto</p>
+          <h1 className={`${styles.title} ${noto.className}`}>Silvia Innocenti</h1>
+          <p className={styles.subtitle}>Presto online</p>
+        </div>
         <div className={styles.logoContainer}>
           <Image
             src="/loading.gif"
